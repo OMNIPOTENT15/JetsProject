41465bc9c50f8a487a9aa979d902174e6b1e090b
Blake Vieyra <blake.vieyra@gmail.com>
2023-12-01 14:55:03 -0800

    inital commit

---
+++ src/com/skilldistillery/jets/JetsApplication.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.jets;
+
+public class JetsApplication {
+	
+	private Airfield airfield;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
